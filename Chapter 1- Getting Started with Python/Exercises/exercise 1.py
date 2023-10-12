d6240ddc3f966314957089d6d3d46e045f5ecfca
KahlilMJPalisocMarcelino <bsukmm2026@gmail.com>
2023-10-12 09:55:24 +0400

    Chapter 1 Exercise 1.py

---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 1.py
@@ -0,0 +1,8 @@
+# This is the solution of exercise 
+
+print("""Twinkle, twinkle, little star,
+          How I wonder what you are!
+              Up above the world so high,
+               Like a diamond in the sky
+        Twinkle, twinkle, little star,
+            How I wonder what you are""")
---
+++ Chapter 1- Getting Started with Python/Exercises/exercise 4.py
@@ -0,0 +1,6 @@
+### Write three strings in different variables and print the output as one string.
+
+movie = "Avatar "
+director  = "James Cameron "
+instrumental = "Nothing Is Lost (You Give Me Strength) "
+print (movie+director+instrumental)
